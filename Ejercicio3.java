e02a4de917c930bf2b692364d07c91dc84ab220c
Diana Alejandra Radu Giju <radudiaale27@gmail.com>
2024-10-31 13:04:31 +0100

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,46 @@
+
+public class Ejercicio3{
+
+        System.out.println("RELACIÓN DE EJERCICIOS 5 - VARIABLES");
+        System.out.println("Ejercicio 3");
+
+        int expresionA;
+        expresionA = 3 / 2 + 4 / 3;
+
+        int expresionB;
+        int x = 0, y = 0;
+        expresionB = 1 / (x - 5) - 3 * x * y / 4;
+
+        int expresionC;
+        expresionC = 1 / 2 + 7;
+
+        int expresionD;
+        expresionD = 7 + 1 / 2;
+
+        int expresionE;
+        int a = 0, b = 0, c = 0, d = 0, e = 0, g = 0, f = 0, h = 0, j = 0;
+        expresionE = a * a / (b - c) + (d - e) / (f - g * h / j);
+
+        int expresionF;
+        int m = 0, n = 0, p = 0;
+        expresionF = m / n + p;
+
+        int expresionG;
+        int q = 0;
+        expresionG = m + n / (p - q);
+
+        int expresionH;
+        expresionH = a * a / (b * b) + c * c / (d * d);
+
+        int expresionI;
+        int r = 0, s = 0;
+        expresionH = (m + n / p) / (q - r / s);
+
+        int expresionJ;
+        expresionJ = (3 * a + b) / (c - (d + 5 * e) / (f + (g / (2 * h))));
+
+        int expresionK;
+        expresionK = (a * a + 2 * a * b + b * b) / (1 / (x * x) + 2);
+        // expresionK = (a * a + 2 * a * b + b * b) / (1 / x * x + 2);
+    }
+}

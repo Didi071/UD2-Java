e0bed1b43863bb7f982c7bcc9937ec09832f8382
Diana Alejandra Radu Giju <radudiaale27@gmail.com>
2024-10-31 13:03:15 +0100

    Eliminar archivo

--- variables.java
+++
@@ -1,9 +0,0 @@
-public class variables {
-    public static void main(String[] var0) {
-        System.out.println( "Declarando variables...");
-        int suma = 0;
-        float sueldo = 2000.68f;
-        System.out.println(suma);
-        System.out.println(sueldo);
-    }
-}
